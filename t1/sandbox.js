9a34423df0580e3fdb8b16b85245b6ace0355651
Sadia Akter Sinthia <sadia15-14369@diu.edu.bd>
2022-03-25 17:41:56 +0600

    Initial commit

---
+++ t1/sandbox.js
@@ -0,0 +1,16 @@
+let age = 25;
+let year = 2019;
+// logs things to console
+console.log(age,year);
+/*comment */
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+//Strings
+console.log("Hello world");
