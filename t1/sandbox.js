da1977192143bee5d30493d2a00edfaba2cbbfc0
Sadia Akter Sinthia <sadia15-14369@diu.edu.bd>
2022-03-25 18:35:38 +0600

    updated

--- t1/sandbox.js
+++ t1/sandbox.js
@@ -14,5 +14,26 @@ console.log(score);
 
 //Strings
 console.log("Hello world");
+let email = "sadia120@diu.edu"
+console.log(email);
 
+//string concatenation
+
+let firstName = 'Sadia';
+let secondName = 'Sinthia';
+
+let fullName = firstName + " " + secondName;
+console.log(fullName)
+//getting characters
+console.log(fullName[2]);
+//string length
+console.log(fullName.length);
+//stirng methods
+
+console.log(fullName.toUpperCase());
+let result = fullName.toUpperCase();
+console.log(result , fullName);
+
+let index = email.indexOf("@")
+console.log(index);
 
